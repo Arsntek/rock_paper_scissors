c3442fbdbb55e5cb3baead4371fb7cc6325839ed
Arsntek <159738116+Arsntek@users.noreply.github.com>
2024-05-31 19:22:06 -0300

    Refactor code to work with UI

--- game.js
+++ game.js
@@ -1,60 +1,72 @@
 function getComputerChoice () {
-  let computerChoice = Math.floor(Math.random() * 3)
-  if (computerChoice === 1) {
-    return 'rock'
-  } else if (computerChoice === 2) {
-    return 'paper'
-  } else {
-    return 'scissors'
-  }
+  const computerOptions = ['rock', 'paper', 'scissors']
+  return computerOptions[Math.floor(Math.random() * 3)]
 }
 
-function getHumanChoice () {
-  do {
-    let humanChoice = window.prompt('Choose rock, paper or scissors')
-    humanChoice = humanChoice.toLowerCase()
-    if (
-      humanChoice.localeCompare('rock') === 0 ||
-      humanChoice.localeCompare('paper') === 0 ||
-      humanChoice.localeCompare('scissors') === 0
-    ) {
-      return humanChoice
+function match (playerChoice, computerChoice, playerScore, computerScore) {
+  playerChoice = playerChoice.toLowerCase()
+  const message = document.querySelector('#message')
+
+  console.log('Player ' + playerChoice)
+  console.log('Computer ' + computerChoice)
+
+  if (playerChoice === computerChoice) {
+    message.textContent = 'Tie'
+  } else if (playerChoice === 'rock') {
+    if (computerChoice === 'paper') {
+      message.textContent = 'Computer Won'
     } else {
-      window.alert('You should choose rock, paper or scissors')
+      message.textContent = 'Player Won'
     }
-  } while (true)
+  } else if (playerChoice === 'scissors') {
+    if (computerChoice === 'rock') {
+      message.textContent = 'Computer Won'
+    } else {
+      message.textContent = 'Player Won'
+    }
+  } else if (playerChoice === 'paper') {
+    if (computerChoice === 'scissors') {
+      message.textContent = 'Computer Won'
+    } else {
+      message.textContent = 'Player Won'
+    }
+  }
+
+  let minivar = message.textContent
+  console.log('MiniVar ' + minivar)
+
+  if (message.textContent === 'Computer Won') {
+    computerScore.textContent = parseInt(computerScore.textContent) + 1
+  } else if (message.textContent === 'Player Won') {
+    playerScore.textContent = parseInt(playerScore.textContent) + 1
+  }
+
+  if (computerScore.textContent === '5') {
+    message.textContent = 'Computer Won the Game. Try Again!'
+    computerScore.textContent = '0'
+    playerScore.textContent = '0'
+  } else if (playerScore.textContent === '5') {
+    message.textContent = 'Player Won the Game. Play Again!'
+    computerScore.textContent = '0'
+    playerScore.textContent = '0'
+  }
 }
 
 function playGame () {
-  let computerChoice = getComputerChoice()
-  let humanChoice = getHumanChoice()
-  if (humanChoice.localeCompare('rock') === 0) {
-    if (computerChoice.localeCompare('rock') === 0) {
-      window.alert('You: ' + humanChoice + ' Computer: ' + computerChoice + ' You tie')
-    } else if (computerChoice.localeCompare('paper') === 0) {
-      window.alert('You: ' + humanChoice + ' Computer: ' + computerChoice + ' You lost')
-    } else {
-      window.alert('You: ' + humanChoice + ' Computer: ' + computerChoice + ' You won')
-    }
-  } else if (humanChoice.localeCompare('paper') === 0) {
-    if (computerChoice.localeCompare('rock') === 0) {
-      window.alert('You: ' + humanChoice + ' Computer: ' + computerChoice + ' You won')
-    } else if (computerChoice.localeCompare('paper') === 0) {
-      window.alert('You: ' + humanChoice + ' Computer: ' + computerChoice + ' You tie')
-    } else {
-      window.alert('You: ' + humanChoice + ' Computer: ' + computerChoice + ' You lost')
-    }
-  } else {
-    if (computerChoice.localeCompare('rock') === 0) {
-      window.alert('You: ' + humanChoice + ' Computer: ' + computerChoice + ' You lost')
-    } else if (computerChoice.localeCompare('paper') === 0) {
-      window.alert('You: ' + humanChoice + ' Computer: ' + computerChoice + ' You won')
-    } else {
-      window.alert('You: ' + humanChoice + ' Computer: ' + computerChoice + ' You tie')
-    }
-  }
-}
+  const playerScore = document.querySelector('#playerScore')
+  const computerScore = document.querySelector('#computerScore')
 
-do {
-  playGame()
-} while (true)
+  playerScore.textContent = '0'
+  computerScore.textContent = '0'
+  console.log('Game started')
+
+  const buttons = document.querySelectorAll('.btnChoice')
+
+  console.log(buttons)
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      match(button.textContent, getComputerChoice(), playerScore, computerScore)
+    })
+  })
+}
+playGame()
